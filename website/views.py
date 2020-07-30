d3d82353b38bfe71d055b31f6ee094656cd3747f
Ann <annphilomina246@gmail.com>
2020-07-30 19:40:41 +0530

    one more view added

--- website/views.py
+++ website/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'index.html')
+    if request.method=='POST':
+        return render(request,'submit.html')
+    else:
+        return render(request,'index.html')
+        
